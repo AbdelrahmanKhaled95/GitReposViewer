e52618fad6840d5efa5ef3f12f7d13750fdb25b4
Abdelrahman Hussein <Abdelrahman.hussein1995@gmail.com>
2021-04-23 13:09:23 +0200

    Add fetch repositories in RepositoryViewModel.

--- GitReposViewer/ReposList/ViewModel/RepositoryListViewModel.swift
+++ GitReposViewer/ReposList/ViewModel/RepositoryListViewModel.swift
@@ -6,3 +6,18 @@
 //
 
 import Foundation
+
+class RepositoryViewModel {
+    
+    let webService: WebServiceProtocol
+    
+    init(webService: WebServiceProtocol) {
+        self.webService = webService
+    }
+    
+    func fetchGitHubRepositories() {
+        webService.getRequest(url: WebRouter.getRepos.url, responseType: RepositoryModel.self) { (RepoList, error) in
+            print(RepoList)
+        }
+    }
+}
