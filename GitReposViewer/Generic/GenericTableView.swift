3cd18e5f235372a12f6fb64f7147281aaca5cc9f
Abdelrahman Hussein <Abdelrahman.hussein1995@gmail.com>
2021-04-23 12:25:18 +0200

    Add Generic UITableView.

---
+++ GitReposViewer/Generic/GenericTableView.swift
@@ -0,0 +1,51 @@
+//
+//  GenericTableView.swift
+//  GitReposViewer
+//
+//  Created by Abdelrahman Hussein on 23/04/2021.
+//
+
+import UIKit
+
+class GenericTableView<Item, Cell: UITableViewCell>: UITableView, UITableViewDataSource, UITableViewDelegate {
+    
+    //MARK:- Properties
+    var items: [Item]
+    var config: (Item, Cell) -> Void
+    var selectHandler: (Item) -> Void
+    //MARK:- Initializer
+    init(frame: CGRect, items: [Item], config: @escaping (Item, Cell) -> Void, selectHandler: @escaping (Item) -> Void) {
+        self.items = items
+        self.config = config
+        self.selectHandler = selectHandler
+        super.init(frame: frame, style: .plain)
+        self.translatesAutoresizingMaskIntoConstraints = false
+        self.dataSource = self
+        self.delegate = self
+        self.register(Cell.self, forCellReuseIdentifier: "Cell")
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    //MARK:- DataSource
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! Cell
+        config(items[indexPath.row], cell)
+        return cell
+    }
+    //MARK:- Delegation
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        selectHandler(items[indexPath.row])
+    }
+}
+//MARK:- Reload Table
+extension GenericTableView {
+    func reloadTable(data items: [Item]) {
+        self.items = items
+        self.reloadData()
+    }
+}
