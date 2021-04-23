2cb0c941b7c0abb3528047403d07cd72866850f1
Abdelrahman Hussein <Abdelrahman.hussein1995@gmail.com>
2021-04-23 14:27:21 +0200

    Filter public repos only.

--- GitReposViewer/ReposList/ViewModel/RepositoryListViewModel.swift
+++ GitReposViewer/ReposList/ViewModel/RepositoryListViewModel.swift
@@ -10,14 +10,32 @@ import Foundation
 class RepositoryViewModel {
     
     let webService: WebServiceProtocol
+    var allReposList: [RepositoryModel] = []
     
     init(webService: WebServiceProtocol) {
         self.webService = webService
     }
     
     func fetchGitHubRepositories() {
-        webService.getRequest(url: WebRouter.getRepos.url, responseType: RepositoryModel.self) { (RepoList, error) in
+        webService.getRequest(url: WebRouter.getRepos.url, responseType: RepositoryModel.self) { [weak self] (RepoList, error) in
+            guard let self = self else { return }
             print(RepoList)
+            if let RepoList = RepoList {
+                self.allReposList = RepoList
+            }
+            
         }
     }
+    //MARK:- RepositoryList Filteration
+    //Step 1: Ensure that each repository is public and not private
+    func getPublicReposOnly(reposList: [RepositoryModel]){
+        var publicRepositoryList: [RepositoryModel] = []
+        for repository in reposList {
+            if repository.repositoryPrivate == false {
+                publicRepositoryList.append(repository)
+            }
+        }
+        getCreationDate(publicReposList: publicRepositoryList)
+    }
+
 }
