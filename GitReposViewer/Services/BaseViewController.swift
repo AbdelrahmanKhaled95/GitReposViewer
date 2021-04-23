c54bd6083c47776890788f047ab06f24ae374ba6
Abdelrahman Hussein <Abdelrahman.hussein1995@gmail.com>
2021-04-24 00:49:02 +0200

    Add dismiss keyboard in baseViewController.

--- GitReposViewer/Services/BaseViewController.swift
+++ GitReposViewer/Services/BaseViewController.swift
@@ -11,8 +11,14 @@ class BaseViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        //Looks for single or multiple taps.
+         let tap = UITapGestureRecognizer(target: self, action: #selector(UIInputViewController.dismissKeyboard))
+        view.addGestureRecognizer(tap)
+    }
+    
+    @objc func dismissKeyboard() {
+        //Causes the view (or one of its embedded text fields) to resign the first responder status.
+        view.endEditing(true)
     }
     
     func showAlert(_ message: String) {
