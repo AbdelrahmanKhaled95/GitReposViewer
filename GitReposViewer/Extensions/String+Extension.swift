7c24e27953ee4cd38856cd6081476fc535946947
Abdelrahman Hussein <Abdelrahman.hussein1995@gmail.com>
2021-04-23 17:51:23 +0200

    Add string extension.

--- GitReposViewer/Extensions/String+Extension.swift
+++ GitReposViewer/Extensions/String+Extension.swift
@@ -6,3 +6,13 @@
 //
 
 import Foundation
+
+extension String {
+    // Convert String to Date
+    func toDate()-> Date? {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZ"
+        let date = dateFormatter.date(from: self)
+        return date
+    }
+}
