a7b0a82d96dd37f5d7eeb680adcba6e11bbcf626
Abdelrahman Hussein <Abdelrahman.hussein1995@gmail.com>
2021-04-23 08:07:03 +0200

    Add GenericTableView.swift

---
+++ GitReposViewer/Generic/GenericTableView.swift
@@ -0,0 +1,20 @@
+//
+//  GenericTableView.swift
+//  GitReposViewer
+//
+//  Created by Abdelrahman Hussein on 23/04/2021.
+//
+
+import UIKit
+
+class GenericTableView: UITableView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
