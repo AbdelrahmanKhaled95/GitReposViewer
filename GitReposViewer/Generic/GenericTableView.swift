4631bb0418dd5f8f744db6668414cf23d61e6c90
Abdelrahman Hussein <Abdelrahman.hussein1995@gmail.com>
2021-04-23 09:45:45 +0200

    Add DataSource to generic table.

--- GitReposViewer/Generic/GenericTableView.swift
+++ GitReposViewer/Generic/GenericTableView.swift
@@ -7,14 +7,31 @@
 
 import UIKit
 
-class GenericTableView: UITableView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+class GenericTableView<Item, Cell: UITableViewCell>: UITableView, UITableViewDataSource {
+    
+    var items: [Item]
+    var config: (Item, Cell) -> Void
+    
+    init(frame: CGRect, items: [Item], config: @escaping (Item, Cell) -> Void) {
+        self.items = items
+        self.config = config
+        super.init(frame: frame, style: .plain)
+        self.dataSource = self
+        self.register(Cell.self, forCellReuseIdentifier: "Cell")
+        
     }
-    */
-
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! Cell
+        config(items[indexPath.row], cell)
+        return cell
+    }
 }
