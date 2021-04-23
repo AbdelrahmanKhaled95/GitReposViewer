372569f17062a7c55cc4cef313790706433fa35b
Abdelrahman Hussein <Abdelrahman.hussein1995@gmail.com>
2021-04-23 07:50:47 +0200

    Rearrange project folders structure.

--- GitReposViewer/ReposList/View/ViewController.swift
+++ GitReposViewer/ReposList/View/ViewController.swift
@@ -8,27 +8,12 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
-    //MARK:-Properties
-    var genericTableView: GenericTableView<String, UITableViewCell>!
-    var data = [String]()
-    
-    //MARK:- Outlets
-    @IBOutlet weak var tableViewContainer: UIView!
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        Timer.scheduledTimer(withTimeInterval: 3.0, repeats: false) { (_) in
-            self.data = Array(repeating: "Abdo", count: 10)
-            self.genericTableView.reloadTable(data: self.data)
-        }
-        genericTableView = GenericTableView(frame: tableViewContainer.bounds, items: data, config: { (item, cell) in
-            cell.textLabel?.text = item
-        }, selectHandler: { (item) in
-            print(item)
-        })
         // Do any additional setup after loading the view.
-        tableViewContainer.addSubview(genericTableView)
     }
+
+
 }
 
