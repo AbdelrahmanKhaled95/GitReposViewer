e81d42295d06a9dad9a1ae52912c585a935364e9
Abdelrahman Hussein <Abdelrahman.hussein1995@gmail.com>
2021-04-23 11:06:28 +0200

    Add Marks in generictableview.

--- GitReposViewer/Generic/GenericTableView.swift
+++ GitReposViewer/Generic/GenericTableView.swift
@@ -13,7 +13,7 @@ class GenericTableView<Item, Cell: UITableViewCell>: UITableView, UITableViewDat
     var items: [Item]
     var config: (Item, Cell) -> Void
     var selectHandler: (Item) -> Void
-    
+    //MARK:- Initializer
     init(frame: CGRect, items: [Item], config: @escaping (Item, Cell) -> Void, selectHandler: @escaping (Item) -> Void) {
         self.items = items
         self.config = config
@@ -28,22 +28,20 @@ class GenericTableView<Item, Cell: UITableViewCell>: UITableView, UITableViewDat
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
+    //MARK:- DataSource
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return items.count
     }
-    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! Cell
         config(items[indexPath.row], cell)
         return cell
     }
-    
+    //MARK:- Delegation
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         selectHandler(items[indexPath.row])
     }
 }
-
 //MARK:- Reload Table
 extension GenericTableView {
     func reloadTable(data items: [Item]) {
