8b037a1afcb9565a2ae18a6216aa4cca7433d857
Abdelrahman Hussein <Abdelrahman.hussein1995@gmail.com>
2021-04-23 13:38:18 +0200

    Create design for Repositories cell.

---
+++ GitReposViewer/ReposList/View/RepositoryCellTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  RepositoryCellTableViewCell.swift
+//  GitReposViewer
+//
+//  Created by Abdelrahman Hussein on 23/04/2021.
+//
+
+import UIKit
+
+class RepositoryCellTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
