2ab4c2c7f2b3da26607c57954597f68355ff0ff2
Abdelrahman Hussein <Abdelrahman.hussein1995@gmail.com>
2021-04-23 13:08:36 +0200

    Add Decoding in fetch repositries.

--- GitReposViewer/Services/WebService.swift
+++ GitReposViewer/Services/WebService.swift
@@ -11,11 +11,26 @@ class WebSerice: WebServiceProtocol {
     
     func getRequest<ResponseType>(url: URL, responseType: ResponseType.Type, completionHandler: @escaping ([ResponseType]?, Error?) -> Void) where ResponseType : Decodable {
         let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
-            
+            guard let data = data else {
+                DispatchQueue.main.async {
+                    completionHandler(nil, error)
+                }
+                return
+            }
+            do {
+                let repoList = try JSONDecoder().decode([ResponseType].self, from: data)
+                DispatchQueue.main.async {
+                    completionHandler(repoList, nil)
+                }
+            } catch(let error) {
+                DispatchQueue.main.async {
+                    completionHandler(nil, error)
+                }
+            }
         }
         task.resume()
     }
     
     
-
+    
 }
