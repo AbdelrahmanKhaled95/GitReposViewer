38ab8e0e3ee2369e85ec3a9e49621b5b5046d129
Abdelrahman Hussein <Abdelrahman.hussein1995@gmail.com>
2021-04-23 11:58:08 +0200

    Add web service protocol.

---
+++ GitReposViewer/Services/WebRouter.swift
@@ -0,0 +1,8 @@
+//
+//  WebRouter.swift
+//  GitReposViewer
+//
+//  Created by Abdelrahman Hussein on 23/04/2021.
+//
+
+import Foundation
---
+++ GitReposViewer/Services/WebService.swift
@@ -0,0 +1,8 @@
+//
+//  WebService.swift
+//  GitReposViewer
+//
+//  Created by Abdelrahman Hussein on 23/04/2021.
+//
+
+import Foundation
