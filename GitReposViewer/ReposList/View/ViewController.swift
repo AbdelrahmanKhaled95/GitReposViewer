a670e2abaaed1fdcc0ebcc7b389e33f9745081ca
Abdelrahman Hussein <Abdelrahman.hussein1995@gmail.com>
2021-04-23 09:53:18 +0200

    Test TableView.

--- GitReposViewer/ReposList/View/ViewController.swift
+++ GitReposViewer/ReposList/View/ViewController.swift
@@ -8,9 +8,14 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var genericTableView: GenericTableView<String, UITableViewCell>!
+    let data = Array(repeating: "Abdo", count: 10)
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        genericTableView = GenericTableView(frame: <#T##CGRect#>, items: <#T##[_]#>, config: <#T##(_, UITableViewCell) -> Void#>, selectHandler: <#T##(_) -> Void#>)
         // Do any additional setup after loading the view.
     }
 
