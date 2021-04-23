52403504c9505dd6790cfff82b91fcc0dbb9ea85
Abdelrahman Hussein <Abdelrahman.hussein1995@gmail.com>
2021-04-23 17:35:25 +0200

    Add custom session.

--- GitReposViewer/Services/WebService.swift
+++ GitReposViewer/Services/WebService.swift
@@ -9,8 +9,22 @@ import Foundation
 
 class WebSerice: WebServiceProtocol {
     
-    func getRequest<ResponseType>(url: URL, responseType: ResponseType.Type, completionHandler: @escaping ([ResponseType]?, Error?) -> Void) where ResponseType : Decodable {
-        let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+    //MARK:- Custom Session
+    var session = URLSession.shared
+    
+    init() {
+        // GitHib Authorization Key
+        let key = "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"
+        let sessionConfig = URLSessionConfiguration.default
+        let authValue: String? = "Bearer \(key)"
+        sessionConfig.httpAdditionalHeaders = ["Authorization": authValue ?? ""]
+        session = URLSession(configuration: sessionConfig, delegate: self as? URLSessionDelegate, delegateQueue: nil)
+    }
+
+    
+    //MARK:- Get Method
+    func getRequest<ResponseType>(url: URL, responseType: ResponseType.Type, completionHandler: @escaping (ResponseType?, Error?) -> Void) where ResponseType : Decodable {
+        let task = session.dataTask(with: url) { (data, response, error) in
             guard let data = data else {
                 DispatchQueue.main.async {
                     completionHandler(nil, error)
@@ -18,12 +32,13 @@ class WebSerice: WebServiceProtocol {
                 return
             }
             do {
-                let repoList = try JSONDecoder().decode([ResponseType].self, from: data)
+                let result = try JSONDecoder().decode(ResponseType.self, from: data)
                 DispatchQueue.main.async {
-                    completionHandler(repoList, nil)
+                    completionHandler(result, nil)
                 }
             } catch(let error) {
                 DispatchQueue.main.async {
+                    print(error)
                     completionHandler(nil, error)
                 }
             }
@@ -31,6 +46,26 @@ class WebSerice: WebServiceProtocol {
         task.resume()
     }
     
-    
-    
+    func getRequestArray<ResponseType: Decodable>(url: URL, responseType: ResponseType.Type, completionHandler: @escaping ([ResponseType]?, Error?) -> Void) {
+        let task = session.dataTask(with: url) { (data, response, error) in
+            guard let data = data else {
+                DispatchQueue.main.async {
+                    completionHandler(nil, error)
+                }
+                return
+            }
+            do {
+                let result = try JSONDecoder().decode([ResponseType].self, from: data)
+                DispatchQueue.main.async {
+                    completionHandler(result, nil)
+                }
+            } catch(let error) {
+                DispatchQueue.main.async {
+                    print(error)
+                    completionHandler(nil, error)
+                }
+            }
+        }
+        task.resume()
+    }
 }
