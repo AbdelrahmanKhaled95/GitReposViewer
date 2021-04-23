3412efb63c5e7541f916a6d755c36a3febd67965
Abdelrahman Hussein <Abdelrahman.hussein1995@gmail.com>
2021-04-23 20:19:40 +0200

    Add cocoapods in project.

---
+++ Pods/Target Support Files/Pods-GitReposViewer-GitReposViewerUITests/Pods-GitReposViewer-GitReposViewerUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_GitReposViewer_GitReposViewerUITests : NSObject
+@end
+@implementation PodsDummy_Pods_GitReposViewer_GitReposViewerUITests
+@end
---
+++ Pods/Target Support Files/Pods-GitReposViewer/Pods-GitReposViewer-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_GitReposViewer : NSObject
+@end
+@implementation PodsDummy_Pods_GitReposViewer
+@end
