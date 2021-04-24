cefd8bdd5a9f079fdbb38a0b7b6dd2d3d4fac137
Abdelrahman Hussein <Abdelrahman.hussein1995@gmail.com>
2021-04-24 04:14:21 +0200

    Add missing properteis.

--- GitReposViewer/RepoDetails/RepoDetailViewController.swift
+++ GitReposViewer/RepoDetails/RepoDetailViewController.swift
@@ -8,13 +8,28 @@
 import UIKit
 
 class RepoDetailViewController: UIViewController {
-
+    
+    @IBOutlet weak var avatarImageView: UIImageView!
+    @IBOutlet weak var ownerNameLabel: UILabel!
+    @IBOutlet weak var creationDateLabel: UILabel!
+    @IBOutlet weak var languageLabel: UILabel!
+    
+    
+    
     //MARK:- Properties
-    var repositoryName, ownerName, ownerAvatar, createionDate: String?
+    var repositoryName, ownerName, ownerAvatar, createionDate, contributors, forks, branches: String?
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
 
         // Do any additional setup after loading the view.
+    }
+    
+    func setupView() {
+        title = repositoryName
+        avatarImageView.loadImage(from: ownerAvatar)
+        ownerNameLabel.text = ownerName
+        creationDateLabel.text = createionDate
     }
 }
