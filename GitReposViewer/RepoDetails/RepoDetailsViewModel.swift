0b4a9a57a57bc0d607abca3d9a8234171133a129
Abdelrahman Hussein <Abdelrahman.hussein1995@gmail.com>
2021-04-24 11:58:53 +0200

    Add RepoDetails VM.

--- GitReposViewer/RepoDetails/RepoDetailsViewModel.swift
+++ GitReposViewer/RepoDetails/RepoDetailsViewModel.swift
@@ -6,3 +6,55 @@
 //
 
 import Foundation
+
+class RepoDetailsViewModel {
+    //MARK:- Properties
+    let webService: WebServiceProtocol
+    var branchListCellViewModel: [BranchesListCellViewModel] = [BranchesListCellViewModel]() {
+        didSet {
+            self.reloadTableView?()
+        }
+    }
+
+    
+    init(webService: WebServiceProtocol = WebSerice()) {
+        self.webService = webService
+    }
+    //MARK:- Binding Closures
+    var reloadTableView: (()->())?
+    var state: ViewState = .empty {
+        didSet {
+            self.updateLoadingStatus?()
+        }
+    }
+    var updateLoadingStatus: (()->())?
+    var errorMessage: String? {
+        didSet {
+            self.showAlert?()
+        }
+    }
+    var showAlert: (()->())?
+    
+    func fetchRepoBranches(ownerName: String, repositoryName: String) {
+        state = .loading
+        webService.getRequestArray(url: WebRouter.getBranches(ownerName, repositoryName).url, responseType: BranchModel.self) { [weak self] (result, error) in
+            guard let self = self else { return }
+            guard let branchList = result else {
+                self.state = .error
+                self.errorMessage = error?.localizedDescription
+                return
+            }
+            self.state = .filled
+            self.genrateBranchListCellVM(branchList: branchList)
+        }
+    }
+    
+    // Process Branch List
+    func genrateBranchListCellVM(branchList: [BranchModel]) {
+        var branchListCellVM: [BranchesListCellViewModel] = []
+        branchList.forEach { (branch) in
+            branchListCellVM.append(BranchesListCellViewModel(branchName: branch.name))
+        }
+        self.branchListCellViewModel = branchListCellVM
+    }
+}
