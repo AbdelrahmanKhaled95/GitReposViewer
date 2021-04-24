e6d242be8ed9349d9af17fe376066d175216a2bd
Abdelrahman Hussein <Abdelrahman.hussein1995@gmail.com>
2021-04-24 20:50:52 +0200

    Add empty state message.

--- GitReposViewer/Extensions/GenericTableView+Extension.swift
+++ GitReposViewer/Extensions/GenericTableView+Extension.swift
@@ -17,4 +17,22 @@ extension GenericTableView {
         guard let cell = self.dequeueReusableCell(withIdentifier: "\(Cell.self)") as? Cell else { fatalError("Can't Happen") }
         return cell
     }
+    
+    func setEmptyMessage(_ message: String) {
+        let messageLabel = UILabel(frame: CGRect(x: 0, y: 0, width: self.bounds.size.width, height: self.bounds.size.height))
+        messageLabel.text = message
+        messageLabel.textColor = .black
+        messageLabel.numberOfLines = 0
+        messageLabel.textAlignment = .center
+        messageLabel.font = UIFont(name: "TrebuchetMS", size: 15)
+        messageLabel.sizeToFit()
+
+        self.backgroundView = messageLabel
+        self.separatorStyle = .none
+    }
+
+    func restore() {
+        self.backgroundView = nil
+        self.separatorStyle = .singleLine
+    }
 }
