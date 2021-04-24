d1f16633c24633fae5718ad68ff4ebbd244de5cf
Abdelrahman Hussein <Abdelrahman.hussein1995@gmail.com>
2021-04-24 16:44:53 +0200

    Add prefetch to Generic collection.

--- GitReposViewer/Generic/GenericCollectionViewController.swift
+++ GitReposViewer/Generic/GenericCollectionViewController.swift
@@ -7,24 +7,28 @@
 
 import UIKit
 
-class GenericCollectionViewController<Item, Cell: UICollectionViewCell>: UICollectionView, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+class GenericCollectionViewController<Item, Cell: UICollectionViewCell>: UICollectionView, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout, UICollectionViewDataSourcePrefetching {
 
     //MARK:- Properties
     var items: [Item]
     var config: (Item, Cell) -> Void
+    var prefetch: () -> Void
     //MARK:- Initializer
-    init(frame: CGRect, items: [Item], config: @escaping (Item, Cell) -> Void) {
+    init(frame: CGRect, items: [Item], config: @escaping (Item, Cell) -> Void, prefetch: @escaping () -> Void) {
         self.items = items
         self.config = config
+        self.prefetch = prefetch
         let layout = UICollectionViewFlowLayout()
         layout.scrollDirection = .horizontal
         super.init(frame: frame, collectionViewLayout: layout)
         layout.itemSize = CGSize(width: self.contentSize.width/5, height: self.contentSize.height/3)
         self.translatesAutoresizingMaskIntoConstraints = false
         self.dataSource = self
+        self.prefetchDataSource = self
         self.registerNib()
         self.backgroundColor = .white
         self.isPagingEnabled = true
+        self.isPrefetchingEnabled = true
         if let flowLayout = self.collectionViewLayout as? UICollectionViewFlowLayout {
             flowLayout.estimatedItemSize = CGSize(width: 1, height: 1)
         }
@@ -42,6 +46,24 @@ class GenericCollectionViewController<Item, Cell: UICollectionViewCell>: UIColle
         guard let cell = self.dequeueReusableCell(withReuseIdentifier: "\(Cell.self)", for: indexPath) as? Cell else { fatalError() }
         config(items[indexPath.row], cell)
         return cell
+    }
+    //MARK:- Prefetching
+    func collectionView(_ collectionView: UICollectionView, prefetchItemsAt indexPaths: [IndexPath]) {
+        for index in indexPaths {
+            if index.row >= items.count - 5 {
+                prefetch()
+            }
+        }
+    }
+    
+
+    
+    func collectionView(_ collectionView: UICollectionView, didEndDisplaying cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
+        print("")
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
+       self.collectionView(collectionView, prefetchItemsAt: [indexPath])
     }
 }
 //MARK:- Reload Table
