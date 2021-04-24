34c8a4813f60709521e8c832378c52d29196ab34
Abdelrahman Hussein <Abdelrahman.hussein1995@gmail.com>
2021-04-24 20:49:27 +0200

    Remove useless properties.

--- GitReposViewer/ReposList/Models/RepositoryModel.swift
+++ GitReposViewer/ReposList/Models/RepositoryModel.swift
@@ -7,79 +7,19 @@
 
 // MARK: - RepositoryModel
 struct RepositoryModel: Decodable {
-    let id: Int
-    let nodeID, name, fullName: String
+    let name: String
     let repositoryPrivate: Bool
     let owner: RepositoryOwnerModel
-    let htmlURL: String
-    let repositoryDescription: String?
-    let fork: Bool
-    let url, forksURL: String
-    let keysURL, collaboratorsURL: String
-    let teamsURL, hooksURL: String
-    let issueEventsURL: String
-    let eventsURL: String
-    let assigneesURL, branchesURL: String
-    let tagsURL: String
-    let blobsURL, gitTagsURL, gitRefsURL, treesURL: String
-    let statusesURL: String
-    let languagesURL, stargazersURL, contributorsURL, subscribersURL: String
-    let subscriptionURL: String
-    let commitsURL, gitCommitsURL, commentsURL, issueCommentURL: String
-    let contentsURL, compareURL: String
-    let mergesURL: String
-    let archiveURL: String
-    let downloadsURL: String
-    let issuesURL, pullsURL, milestonesURL, notificationsURL: String
-    let labelsURL, releasesURL: String
-    let deploymentsURL: String
+    let forksURL, contributorsURL, branchesURL : String
+
     
     enum CodingKeys: String, CodingKey {
-        case id
-        case nodeID = "node_id"
         case name
-        case fullName = "full_name"
         case repositoryPrivate = "private"
         case owner
-        case htmlURL = "html_url"
-        case repositoryDescription = "description"
-        case fork, url
         case forksURL = "forks_url"
-        case keysURL = "keys_url"
-        case collaboratorsURL = "collaborators_url"
-        case teamsURL = "teams_url"
-        case hooksURL = "hooks_url"
-        case issueEventsURL = "issue_events_url"
-        case eventsURL = "events_url"
-        case assigneesURL = "assignees_url"
         case branchesURL = "branches_url"
-        case tagsURL = "tags_url"
-        case blobsURL = "blobs_url"
-        case gitTagsURL = "git_tags_url"
-        case gitRefsURL = "git_refs_url"
-        case treesURL = "trees_url"
-        case statusesURL = "statuses_url"
-        case languagesURL = "languages_url"
-        case stargazersURL = "stargazers_url"
         case contributorsURL = "contributors_url"
-        case subscribersURL = "subscribers_url"
-        case subscriptionURL = "subscription_url"
-        case commitsURL = "commits_url"
-        case gitCommitsURL = "git_commits_url"
-        case commentsURL = "comments_url"
-        case issueCommentURL = "issue_comment_url"
-        case contentsURL = "contents_url"
-        case compareURL = "compare_url"
-        case mergesURL = "merges_url"
-        case archiveURL = "archive_url"
-        case downloadsURL = "downloads_url"
-        case issuesURL = "issues_url"
-        case pullsURL = "pulls_url"
-        case milestonesURL = "milestones_url"
-        case notificationsURL = "notifications_url"
-        case labelsURL = "labels_url"
-        case releasesURL = "releases_url"
-        case deploymentsURL = "deployments_url"
     }
 }
 
