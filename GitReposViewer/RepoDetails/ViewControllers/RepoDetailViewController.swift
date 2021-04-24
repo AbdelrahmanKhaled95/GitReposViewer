1664a7a077c9b6f8a812bab22c594440942829a8
Abdelrahman Hussein <Abdelrahman.hussein1995@gmail.com>
2021-04-24 11:53:37 +0200

    Add missing outlets container.

--- GitReposViewer/RepoDetails/ViewControllers/RepoDetailViewController.swift
+++ GitReposViewer/RepoDetails/ViewControllers/RepoDetailViewController.swift
@@ -9,21 +9,35 @@ import UIKit
 
 class RepoDetailViewController: UIViewController {
     
+    //MARK:- Outlets
     @IBOutlet weak var avatarImageView: UIImageView!
     @IBOutlet weak var ownerNameLabel: UILabel!
     @IBOutlet weak var creationDateLabel: UILabel!
     @IBOutlet weak var languageLabel: UILabel!
-    
+    @IBOutlet weak var contributorViewContainer: UIView!
+    @IBOutlet weak var branchListTableViewController: UIView!
+    @IBOutlet weak var forkCollectionViewController: UIView!
     
     
     //MARK:- Properties
     var repositoryName, ownerName, ownerAvatar, createionDate, contributors, forks, branches: String?
+    var genericTableView: GenericTableView<BranchesListCellViewModel, UITableViewCell>?
+    lazy var viewModel: RepoDetailsViewModel = {
+        return RepoDetailsViewModel()
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         setupView()
-
+        setupBinding()
         // Do any additional setup after loading the view.
+    }
+    
+    func setupGenericTableView() {
+        genericTableView = GenericTableView(frame: branchListTableViewController.bounds, items: viewModel.branchListCellViewModel, config: configRepoTable, selectHandler: selectHelper)
+        if let genericTableView = genericTableView{
+            branchListTableViewController.addSubview(genericTableView)
+        }
     }
     
     func setupView() {
