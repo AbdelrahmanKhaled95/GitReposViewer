0b542f799e096b92fd9ad6e048c47c6be84d302d
Abdelrahman Hussein <Abdelrahman.hussein1995@gmail.com>
2021-04-24 19:40:53 +0200

    Add RepoListVMTests.

---
+++ GitReposViewerTests/RepoListViewModelTest.swift
@@ -0,0 +1,13 @@
+//
+//  RepoListViewModelTest.swift
+//  GitReposViewerTests
+//
+//  Created by Abdelrahman Hussein on 24/04/2021.
+//
+
+import XCTest
+@testable import GitReposViewer
+
+class RepoListViewModelTests: XCTestCase {
+    
+}
